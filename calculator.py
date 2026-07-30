b15abaca410d92174d8c7f5f16fbc1869c98aa18
keshavbedia <77234370+keshavbedia@users.noreply.github.com>
2026-07-30 15:15:42 +0530

    Add functions for addition and multiplication

---
+++ calculator.py
@@ -0,0 +1,7 @@
+def add_numbers(a: int, b: int) -> int:
+    """Adds two numbers together."""
+    return a + b
+
+def multiply_numbers(a: int, b: int) -> int:
+    """Multiplies two numbers."""
+    return a * b
